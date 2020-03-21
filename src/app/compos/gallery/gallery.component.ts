cfd6391d434122023736c2e2c1eefa76cfe37268
sujan manandhar <sujanmanandhar@sujans-Air.lan>
2020-03-21 10:00:59 -0500

    openClosedComp

--- src/app/compos/gallery/gallery.component.ts
+++ src/app/compos/gallery/gallery.component.ts
@@ -20,17 +20,12 @@ export class GalleryComponent implements OnInit {
   @Input() subCitySelect: string;
   @Input() priceRangeSelect: number;
   errMessage: string;
-  openingTime: string;
-  closingTime: string;
-  closedToday = false;
-  nextOpenDay: string;
 
   products$ = this.productService.products$
     .pipe(
         map((products) => {
           return products.map((prod) => ({
-            ...prod,
-            isOpen: this.checkIfOpen(prod.storeHours),
+            ...prod
           }) as ProductsModel);
         }),
         catchError(err => this.errMessage = err)
@@ -44,51 +39,5 @@ export class GalleryComponent implements OnInit {
 
   getProdDetails(id: string) {
     this.router.navigate(['/product-info', id]);
-  }
-
-  checkIfOpen(storeHours): boolean {
-    const date = new Date();
-    const day = date.getDay();
-    const hour = date.getHours();
-    let hoursArr = [];
-    // creating new Object with sorted days in a week
-    const sortedWeek = {
-      monday: storeHours.monday,
-      tuesday: storeHours.tuesday,
-      wednesday: storeHours.wednesday,
-      thursday: storeHours.thursday,
-      friday: storeHours.friday,
-      saturday: storeHours.saturday,
-      sunday: storeHours.sunday,
-    };
-    const sortedKeys = Object.keys(sortedWeek);
-    for (let i = 0; i < sortedKeys.length; i++) {
-      if (day === i + 1) {
-        const dayHours = storeHours[sortedKeys[i]];
-        if (dayHours === 'closed') {
-          // add a function that takes the storeHours and sortedKeys and figure out
-          // opening and closing time
-          const nextDayArr = storeHours[sortedKeys[i + 1]].split('-');
-          const amPm = (nextDayArr) ? 'AM' : 'PM';
-          this.closedToday = true;
-          this.nextOpenDay = `${sortedKeys[i + 1]} ${nextDayArr[0]} ${amPm}`;
-          return false;
-        } else {
-          hoursArr = dayHours.split('-');
-          this.openingTime = hoursArr[0];
-          this.closingTime = hoursArr[1];
-          const openingTime = parseInt(hoursArr[0], 10);
-          const closingTime = parseInt(hoursArr[1], 10) + 12;
-
-          if (hour < openingTime) {
-            return false;
-          } else if (hour >= openingTime && hour < closingTime) {
-            return true;
-          }
-        }
-
-      }
-    }
-
   }
 }
