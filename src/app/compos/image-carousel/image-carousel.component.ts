4b0d3f77b5c36ea7ea96cb583cff5f20511599a3
sujan manandhar <sujanmanandhar@sujans-Air.lan>
2021-02-21 16:23:41 -0600

    need to figure

--- src/app/compos/image-carousel/image-carousel.component.ts
+++ src/app/compos/image-carousel/image-carousel.component.ts
@@ -11,15 +11,38 @@ export class ImageCarouselComponent implements OnInit {
   noOfImages = Math.floor(window.innerWidth / this.imageSize);
   slideRight = false;
   slideLeft = false;
+  private prevDisplayImg: any;
+  private displayImg = [];
+  private loopEnd: number;
+  private nextClicked = false;
+  imgTimeCount = 0;
+  nextDisplayImg = [];
   constructor() { }
 
   ngOnInit() {
-    console.log('image', this.images);
-    console.log('wef', this.noOfImages);
+    const loopStart = 0; const loopEnd = this.noOfImages;
+    this.loopIt(loopStart, loopEnd, this.displayImg);
+    this.prevDisplayImg = this.displayImg;
+    console.log('@@@', this.images.length);
+  }
+
+  loopIt(loopStart: number, loopEnd: number, array): void {
+    // array = [];
+    const imageLength = this.images.length;
+    loopEnd = (loopEnd > imageLength) ? imageLength : loopEnd;
+    for (let i = loopStart; i < loopEnd; i++) {
+      array.push(this.images[i]);
+    }
+    this.loopEnd = loopEnd;
+    console.log('DI', this.displayImg);
+    console.log('DI', this.nextDisplayImg);
   }
 
   navImg(direction: string): void {
     if (direction === 'next') {
+      this.nextClicked = true;
+      this.imgTimeCount++;
+      this.loopIt(this.loopEnd, (this.loopEnd + (this.noOfImages * this.imgTimeCount)), this.nextDisplayImg);
       this.slideLeft = false;
       this.slideRight = true;
     } else {
